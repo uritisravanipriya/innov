0d4e9a39dc960e25d714a4db019a54f49963d790
uritisravanipriya <uritisravanipriya@gmail.com>
2016-04-06 16:24:33 -0400

    hi mahesh

---
+++ Addition.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package addition;
+
+import java.util.Scanner;
+
+
+public class Addition {
+
+    
+    public static void main(String[] args) {
+   
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter the no of rows");
+        int rows=s.nextInt();
+        System.out.println("enter the no of columns");
+        int columns=s.nextInt();
+        int [][] a=new int[rows][columns];
+        int [][] b=new int[rows][columns];
+        
+        
+        
+        
+        
+        
+        
+    }
+}
